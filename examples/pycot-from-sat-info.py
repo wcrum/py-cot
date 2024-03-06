08f321281c1c82f5c9114db1713cb9d323aa706e
Will <30413278+wcrum@users.noreply.github.com>
2024-03-06 08:00:37 -0700

    Update Satellite Example

--- examples/pycot-from-sat-info.py
+++ examples/pycot-from-sat-info.py
@@ -6,9 +6,6 @@ from skyfield.api import load, utc, wgs84
 
 import CoT
 
-ts = load.timescale()
-t = ts.now()
-
 now = datetime.now(timezone.utc)
 stale = now + timedelta(minutes=2)
 
@@ -16,38 +13,36 @@ stations_url = "http://celestrak.com/NORAD/elements/gps-ops.txt"
 satellites = load.tle_file(stations_url)
 
 TAK_IP = "localhost"
-TAK_PORT = 8999
+TAK_PORT = 1234
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((TAK_IP, TAK_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-d = datetime.now()
-d = d.replace(tzinfo=utc)
 ts = load.timescale()
 
 while True:
-    t = ts.from_datetime(d)
+    t = ts.now()
     for sat in satellites:
         geocentric = sat.at(t)
         lat, lon = wgs84.latlon_of(geocentric)
 
-        satCoT = CoT.Event(
-            type="a-u-G-U-U-S-R-S",
-            uid=sat.name,
-            access="Unclassified",
-            how="h-e",
-            time=now,
-            start=now,
-            stale=stale,
-            point=CoT.Point(
-                lat=lat.degrees,
-                lon=lon.degrees,
-                hae=90,
-                le=90,
-                ce=90,
-            ),
-            detail={"contact": {"callsign": sat.name}},
-        )
-        sock.sendall(bytes(satCoT.xml(), encoding="utf-8"))
-
-    time.sleep(10)
+        try:
+            satCoT = CoT.Event(
+                type="a-u-G-U-U-S-R-S",
+                uid=sat.name,
+                access="Unclassified",
+                how="h-e",
+                time=now,
+                start=now,
+                stale=stale,
+                point=CoT.Point(
+                    lat=lat.degrees,
+                    lon=lon.degrees,
+                    hae=0,
+                    le=0,
+                    ce=0,
+                ),
+                detail={"contact": {"callsign": sat.name}},
+            )
+            sock.sendto(bytes(satCoT.xml(), encoding="utf-8"), ((TAK_IP, TAK_PORT)))
+        except:
+            print("Bad satellite TLE data.")

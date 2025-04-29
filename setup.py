28f932088de96214464eeaf082de763654f1b6b9
William Crum <will@wcrum.dev>
2025-04-29 08:19:59 -0600

    update license and release

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+__title__ = 'pycot'
+__version__ = '3.0.0'
+__author__ = 'William Crum'
+__license__ = 'Apache 2.0 License'
+
 setup(
     name="PyCoT",
-    version="0.3",
+    version="3.0.0",
     description="Python Cursor On Target Object-Relational Mapper",
     author="William Crum",
     author_email="will@wcrum.dev",
+    url=f'https://github.com/ampledata/{__title__}',
     packages=["CoT", "CoT.models", "CoT.xml"],
     install_requires=[
         "pydantic>=1.10.2",
